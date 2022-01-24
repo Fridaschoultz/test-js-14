d784d154e887a378dfad97944d602b41bb1aa497
fridaschoultz <fridaschoultz@hotmail.com>
2022-01-24 21:17:36 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,84 @@
+//addition
+/*
+let x = 30;
+let b = 10;
+let y = x+b;
+document.write("Summan är = ", y);
+*/
+
+//subtraktion
+/*
+let x = 30;
+let b = 10;
+let y = x-b;
+document.write("Summan är = ", x-b);
+*/
+
+//gånger
+/*
+let x = 30;
+let b = 10;
+let y = x*b;
+document.write("Summan är = ",y );
+*/
+
+//Exponential tal, upphöjt till
+/*
+let x = 5;
+let b = 2;
+let y = x**b;
+document.write("Summan är = ",y );
+*/
+
+// Division
+/*
+let x = 9;
+let b = 3;
+let y = x/b;
+document.write("Summan är = ",y );
+*/
+
+// Modulus
+/*
+let x = 6;
+let b = 5;
+let y = x%b;
+document.write("Modulus är = ",y );
+*/
+
+//Ökning
+/*
+let x = 5;
+x++;
+let z = x;
+document.write("Summan är: ", z)
+*/
+
+//Sänkning
+/*
+let x = 5;
+x--;
+let z = x;
+document.write("Summan är: ", z)
+*/
+
+// getElementById
+/*
+let a = 10;
+let b = 20;
+let x = a+b;
+document.getElementById("test").innerHTML="the value of x is: " + x
+*/
+
+//document.write()- i webbläsaren
+//document.write("The sum is ", 10 + 10);
+
+//alert i webbläsaren
+//window.alert("Hello World!");
+
+// console.log i konsolen i dev tools
+/*
+let a = 5;
+let b = 15;
+console.log("The value of a + b is", a + b);
+*/
